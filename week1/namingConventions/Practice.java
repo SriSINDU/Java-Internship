e881f6a2e3698242d9bac2e9972f178432c8c74b
SriSINDU <srisindu.3@gmail.com>
2019-05-15 17:01:11 +0530

    adding text

---
+++ week1/namingConventions/Practice.java
@@ -0,0 +1,12 @@
+package namingConventions;
+
+public class Practice {
+
+    public static void main(String[] args){
+        int var1;
+        const int MAX_VALUE=10;
+        var1=2;
+        System.out.println(var1+MAX_VALUE);
+
+    }
+}
